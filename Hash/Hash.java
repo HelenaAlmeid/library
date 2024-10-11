2b1b3ade26ce1e3958a802caeb307ad9032290fc
HelenaAlmeid <165021004+HelenaAlmeid@users.noreply.github.com>
2024-10-11 12:17:18 -0300

    .

---
+++ Hash/Hash.java
@@ -0,0 +1,46 @@
+package Hash;
+
+public class Hash {
+    private int tamanho;
+    private int[] num;
+    
+    public Hash(int tamanho){
+        this.tamanho = tamanho;
+        this.num = new int[tamanho];
+    }
+
+    public int hash(int valor){
+        if(valor < tamanho  && valor > 0){
+            return valor;
+        }
+        boolean verificar = false;
+        int valorMudificado = valor;
+        while(verificar){
+            if(valorMudificado % 2 == 0){
+                valorMudificado = valor /2;
+                if(valorMudificado < tamanho && valorMudificado > 0){
+                    verificar = true;
+                }
+            }
+            else{
+                valorMudificado = valor / 3;
+                if(valorMudificado < tamanho && valorMudificado > 0){
+                    verificar = true;
+                }
+
+            }
+        }
+
+        return valorMudificado;
+        
+    }
+
+    public void adicionarNumero(int valor){
+        int chave = hash(valor);
+        if(num[chave] != 0){
+
+
+        }
+    }
+    
+}
